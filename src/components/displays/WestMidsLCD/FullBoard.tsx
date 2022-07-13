eca283507eeb15f45d8e8b913b93aae9d05d1eb1
David Wheatley <hi@davwheat.dev>
2022-07-13 03:29:22 +0100

    fix: gracefully handle API errors

--- src/components/displays/WestMidsLCD/FullBoard.tsx
+++ src/components/displays/WestMidsLCD/FullBoard.tsx
@@ -4,14 +4,13 @@ import GetNextTrainsAtStation, { ApiResponse } from '../../../api/GetNextTrainsA
 
 import './css/board/index.less';
 
-import clsx from 'clsx';
 import { debounce } from 'throttle-debounce';
 import BoardHeader from './BoardHeader';
 import NextTrain from './NextTrainData';
 
 interface IProps {
   station: string;
-  // noBg: boolean;
+  platformNumber: number;
 }
 
 function loadTrainData(station: string, setTrainData: (data: any) => void) {
@@ -45,39 +44,39 @@ function isValidResponseApi(response: ApiResponse | null | { error: true }): res
   return response !== null && !(response as any).error;
 }
 
-function FullBoard({ station, platformNumber }: { station: string; platformNumber: number }) {
+function FullBoard({ station, platformNumber }: IProps) {
   const [trainData, setTrainData] = useState<ApiResponse | null | { error: true }>(null);
 
   const isError = !isValidResponseApi(trainData);
 
-  const [loadingData, setLoadingData] = useState(false);
-  const [lastUpdated, setLastUpdated] = useState(0);
+  const [dataInfo, setDataInfo] = useState({
+    loadingData: false,
+    lastUpdated: 0,
+  });
 
   const boardRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (loadingData) return;
+    if (dataInfo.loadingData) return;
 
-    if (lastUpdated === 0) {
+    if (dataInfo.lastUpdated === 0) {
       loadTrainData(station, (data) => {
         setTrainData(data);
-        setLastUpdated(Date.now());
-        setLoadingData(false);
+        setDataInfo({ lastUpdated: Date.now(), loadingData: false });
       });
     }
 
     const key = setInterval(() => {
       loadTrainData(station, (data) => {
         setTrainData(data);
-        setLastUpdated(Date.now());
-        setLoadingData(false);
+        setDataInfo({ lastUpdated: Date.now(), loadingData: false });
       });
     }, UPDATE_INTERVAL_SECS * 1000);
 
     return () => {
       clearInterval(key);
     };
-  }, [setTrainData, setLastUpdated, setLoadingData, lastUpdated, loadingData, station, loadTrainData]);
+  }, [setTrainData, setDataInfo, dataInfo, station, loadTrainData]);
 
   useEffect(() => {
     boardRef.current && requestAnimationFrame(() => fillDiv(boardRef.current!));
@@ -105,9 +104,39 @@ function FullBoard({ station, platformNumber }: { station: string; platformNumbe
   const secondService = !isError && trainData.trainServices?.[1];
   const thirdService = !isError && trainData.trainServices?.[2];
 
+  if (isError) {
+    return (
+      <article className="tfwm-board tfwm-board__notice" ref={boardRef}>
+        <BoardHeader platformNumber={platformNumber} stationName={station} />
+
+        <div className="fullscreenNotice">
+          <p>Please listen for announcements or call National&nbsp;Rail&nbsp;Enquiries on 03457 48 49 50</p>
+        </div>
+      </article>
+    );
+  }
+
+  if (dataInfo.loadingData) {
+    return (
+      <article className="tfwm-board tfwm-board__notice" ref={boardRef}>
+        <BoardHeader platformNumber={platformNumber} stationName={station} />
+      </article>
+    );
+  }
+
+  if (trainData.trainServices?.length === 0) {
+    <article className="tfwm-board tfwm-board__notice" ref={boardRef}>
+      <BoardHeader platformNumber={platformNumber} stationName={station} />
+
+      <div className="fullscreenNotice">
+        <p>Please listen for announcements or call National&nbsp;Rail&nbsp;Enquiries on 03457 48 49 50</p>
+      </div>
+    </article>;
+  }
+
   return (
     <article className="tfwm-board" ref={boardRef}>
-      <BoardHeader platformNumber={platformNumber} stationName={isError ? 'Loading...' : trainData.locationName} />
+      <BoardHeader platformNumber={platformNumber} stationName={trainData?.locationName ?? station} />
 
       {firstService && <NextTrain nextTrain={firstService} />}
     </article>
