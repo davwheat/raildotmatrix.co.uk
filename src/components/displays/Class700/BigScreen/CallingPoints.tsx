9bcb0fb065fcefdeee01e97c84517811ea07b63e
David Wheatley <hi@davwheat.dev>
2022-08-17 07:28:21 +0100

    feat: use url params for calling points

--- src/components/displays/Class700/BigScreen/CallingPoints.tsx
+++ src/components/displays/Class700/BigScreen/CallingPoints.tsx
@@ -9,14 +9,40 @@ export default function CallingPointsBigScreen() {
   destination &&= crsToStationName(destination);
   destination ??= 'Unknown';
 
+  let callingPoints: string[] = getUrlParam('stop') ?? [];
+
+  if (typeof callingPoints === 'string') {
+    callingPoints = [callingPoints];
+  } else if (!Array.isArray(callingPoints)) {
+    callingPoints = [];
+  }
+
+  callingPoints = callingPoints.map(crsToStationName).filter(Boolean) as string[];
+
+  if (callingPoints.length === 0) {
+    callingPoints = [destination];
+  } else {
+    callingPoints.push(destination);
+  }
+
   return (
     <div className="callingPoints splitLines">
       <div className="darkBlue">
         <span className="text t900">{destination}</span>
       </div>
       <SlideyScrollText className="lightBlue">
-        <span className="text">Calling at Hassocks, Burgess Hill, Preston Park and Brighton.</span>
+        <span className="text">{pluralise(callingPoints)}</span>
       </SlideyScrollText>
     </div>
   );
+}
+
+function pluralise(arr: string[]): string {
+  if (arr.length === 0) return '';
+
+  if (arr.length === 1) {
+    return `Calling at ${arr[0]} only.`;
+  }
+
+  return `Calling at ${arr.slice(0, -1).join(', ')} and ${arr[arr.length - 1]}.`;
 }
