b38ee0145dddfa70a1e95b0e8ae03808d733f3ec
David Wheatley <david@davwheat.dev>
2023-12-23 00:32:51 +0000

    feat: get services staff via proxy function

---
+++ src/api/GetNextTrainsAtStationStaff.ts
@@ -0,0 +1,50 @@
+import { StaffServicesResponse } from '../../functions/api/getServices';
+
+export { StaffServicesResponse };
+interface IOptions {
+  count?: number;
+  timeWindow?: number;
+  minOffset: number;
+  mustStopAt?: string | null;
+}
+
+export default async function GetNextTrainsAtStationStaff(
+  station: string,
+  options: IOptions = { count: 3, timeWindow: 120, minOffset: 0, mustStopAt: null },
+  abortController?: AbortController
+): Promise<StaffServicesResponse | null | { error: true }> {
+  // return require('./ExampleStaffResponse.json');
+
+  if (options.minOffset < -239) {
+    console.error('Time offset cannot be more than 239 minutes in the past.');
+    return null;
+  }
+  if (options.minOffset > 119) {
+    console.error('Time offset cannot be more than 119 minutes in the future.');
+    return null;
+  }
+
+  const params = new URLSearchParams({
+    station,
+    count: options?.count?.toString() || '10',
+    timeWindow: options?.timeWindow?.toString() || '120',
+    minOffset: options?.minOffset?.toString() || '0',
+  });
+
+  let response: Response;
+
+  try {
+    response = await fetch(`/api/getServices?${params}`, {
+      signal: abortController ? abortController.signal : undefined,
+    });
+  } catch (e) {
+    console.error(e);
+    return { error: true };
+  }
+
+  if (response.ok === false) {
+    return { error: true };
+  }
+
+  return await response.json();
+}
