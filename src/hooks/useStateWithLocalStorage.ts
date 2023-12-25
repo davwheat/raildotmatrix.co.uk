24eb84755728606ad5965618d0edba1593f8cdfa
David Wheatley <david@davwheat.dev>
2023-12-25 08:16:02 +0000

    chore: convert to typescript

--- src/hooks/useStateWithLocalStorage.ts
+++ src/hooks/useStateWithLocalStorage.ts
@@ -1,24 +1,46 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function useStateWithLocalStorage(key, initialValue) {
+/**
+ * Wrapper for React's `useState` hook that persists data in the browser's LocalStorage.
+ *
+ * The value must be `JSON.stringify`/`parse`-able in order to successfully store itself
+ * in LocalStorage.
+ *
+ * Can accept an optional third parameter to validate the persisted value of the state
+ * to ensure it is acceptable before it gets loaded as the initial state. This should be
+ * prepared to accept any value as input, and should return a boolean: `true` meaning
+ * valid and `false` meaning invalid.
+ */
+export default function useStateWithLocalStorage<T>(
+  key: string,
+  initialValue: T,
+  validator: (input: any) => boolean = () => true
+): [T, React.Dispatch<React.SetStateAction<T>>, () => void] {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState(() => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') return initialValue;
+
     try {
-      // Get from local storage by key
       const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+
+      if (item) {
+        const parsed = JSON.parse(item);
+        if (validator(parsed)) {
+          return parsed;
+        }
+      }
+      return initialValue;
     } catch (error) {
       // If error also return initialValue
-      //console.error(error)
+      console.error(error);
       return initialValue;
     }
   });
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = (value) => {
+  const setValue: React.Dispatch<React.SetStateAction<T>> = (value) => {
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore = value instanceof Function ? value(storedValue) : value;
@@ -32,6 +54,10 @@ function useStateWithLocalStorage(key, initialValue) {
     }
   };
 
+  /**
+   * Resets the stored and actual state back to the initial value provided
+   * to the hook call.
+   */
   const resetValue = () => {
     try {
       // Allow value to be a function so we have same API as useState
@@ -48,5 +74,3 @@ function useStateWithLocalStorage(key, initialValue) {
 
   return [storedValue, setValue, resetValue];
 }
-
-export default useStateWithLocalStorage;
