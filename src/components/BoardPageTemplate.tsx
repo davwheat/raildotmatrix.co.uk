09f9baabecd607d821e57992a8c5d6b3007202ea
David Wheatley <david@davwheat.dev>
2024-02-22 00:30:24 +0000

    feat: move to page-per-board

---
+++ src/components/BoardPageTemplate.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link, PageProps, navigate } from 'gatsby';
+import NoSSR from '@mpth/react-no-ssr';
+
+interface BoardPageSettings {
+  requireStation: boolean;
+}
+
+export default function createBoardPage(Component: React.ComponentType<any>, { requireStation = true }: BoardPageSettings) {
+  return function BoardPageTemplate({ location: { search } }: PageProps) {
+    const urlParams = new URLSearchParams(search);
+
+    const attrs = {
+      editBoardCallback: () => {
+        navigate('/board');
+      },
+      station: urlParams.get('station') || '',
+    };
+
+    if (requireStation && attrs.station === '') {
+      return (
+        <NoSSR>
+          <div>
+            <p>Invalid station ({attrs.station || '<none>'}).</p>
+            <Link to="/board">Edit board</Link>
+          </div>
+        </NoSSR>
+      );
+    }
+
+    return (
+      <NoSSR>
+        <Component {...attrs} />
+      </NoSSR>
+    );
+  };
+}
--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -9,51 +9,30 @@ import Form, { AutocompleteSelect, Select } from '../components/common/form';
 
 import Attribution from '../components/common/Attribution';
 
-import NewGTR from '../components/displays/NewGTR';
-import WestMidsLCD from '../components/displays/WestMidsLCD';
-import Class700PIS from '../components/displays/Class700';
-import NoSSR from '@mpth/react-no-ssr';
+import type { PageProps } from 'gatsby';
 
-function isValidBoard(BoardSettings) {
-  const boardName = BoardSettings.type;
-  const station = BoardSettings.station;
-
-  if (['gtr-new', 'tfwm-lcd'].includes(boardName) && station) {
-    return true;
-  } else if (boardName === 'class-700') {
-    return true;
-  }
-
-  return false;
-}
-
-const IndexPage = () => {
+export default function IndexPage({ location: { search } }: PageProps) {
   const [autocomplete, setAutocomplete] = useState([{ label: 'Loading stations...', value: 'VIC' }]);
 
-  let searchParams;
-
-  if (typeof window !== 'undefined') {
-    searchParams = window && new URLSearchParams(window.location.search);
-  }
-
-  const stn = searchParams?.get('station');
-  const type = searchParams?.get('type');
+  const searchParams = new URLSearchParams(search);
+  const stn = searchParams.get('station');
+  const type = searchParams.get('type');
 
   const [BoardSettings, setBoardSettings] = useState({
     station: stn || '',
-    type: type || 'gtr-new',
+    type: type || 'infotec-landscape-dmi',
   });
 
-  const [Page, setPage] = useState(isValidBoard(BoardSettings) ? 1 : 0);
+  function ChooseStation(stn: { label: string; value: string }) {
+    console.log(stn);
 
-  function ChooseStation(stn) {
     setBoardSettings({
       type: BoardSettings.type,
       station: stn.value,
     });
   }
 
-  function ChooseDisplay(display) {
+  function ChooseDisplay(display: { label: string; value: string }) {
     setBoardSettings({
       type: display.target.value,
       station: BoardSettings.station,
@@ -74,83 +53,42 @@ const IndexPage = () => {
     }
   }, [autocomplete, setAutocomplete]);
 
-  const Board = getBoard(BoardSettings, setPage);
-
-  if (!Board && Page === 1) {
-    // Reset when invalid board type is selected
-    setPage(0);
-  }
-
   return (
     <Layout>
-      <NoSSR>
-        <Seo title={Page === 0 ? 'Board Setup' : 'Station Board'} />
-        {Page === 0 && (
-          <main>
-            <header>
-              <TypewriterText component="h1" className="display" cursor text="Board settings" time={500} />
-            </header>
-            <article>
-              <Form>
-                <AutocompleteSelect
-                  onChange={ChooseStation}
-                  label="Select a station"
-                  autocompleteOptions={autocomplete}
-                  value={BoardSettings.station.value}
-                />
-                <Select
-                  label="Display type"
-                  options={[
-                    { value: 'gtr-new', label: 'Infotec DMI' },
-                    { value: 'tfwm-lcd', label: 'Blackbox horizontal LCD' },
-                  ]}
-                  placeholder="Choose a display"
-                  onChange={ChooseDisplay}
-                  value={BoardSettings.type}
-                />
-                <PageLink
-                  onClick={() => {
-                    if (!BoardSettings.station || !BoardSettings.type) return false;
-                    else return true;
-                  }}
-                  afterExit={() => setPage(1)}
-                  style={{ cursor: 'pointer' }}
-                >
-                  Next
-                </PageLink>
-              </Form>
-            </article>
-          </main>
-        )}
-
-        {Page === 1 && Board}
-      </NoSSR>
+      <Seo title="Choose dpearture board" />
+      <main>
+        <header>
+          <TypewriterText component="h1" className="display" cursor text="Board settings" time={500} />
+        </header>
+        <article>
+          <Form>
+            <AutocompleteSelect
+              onChange={ChooseStation as any}
+              label="Select a station"
+              autocompleteOptions={autocomplete}
+              value={BoardSettings.station.value}
+            />
+            <Select
+              label="Display type"
+              options={[
+                { value: 'infotec-landscape-dmi', label: 'Infotec landscape DMI' },
+                { value: 'blackbox-landscape-lcd', label: 'Blackbox landscape LCD' },
+              ]}
+              placeholder="Choose a display"
+              onChange={ChooseDisplay as any}
+              value={BoardSettings.type}
+            />
+            <PageLink
+              to={BoardSettings.station && BoardSettings.type ? `/board/${BoardSettings.type}?station=${BoardSettings.station}` : undefined}
+              style={{ cursor: 'pointer' }}
+            >
+              Next
+            </PageLink>
+          </Form>
+        </article>
+      </main>
 
       <Attribution />
     </Layout>
   );
-};
-
-function getBoard(BoardSettings, setPage) {
-  const boardName = BoardSettings.type;
-
-  const attrs = {
-    editBoardCallback: () => {
-      setPage(0);
-    },
-    station: BoardSettings.station,
-  };
-
-  if (boardName === 'gtr-new') {
-    return <NewGTR {...attrs} />;
-  } else if (boardName === 'tfwm-lcd') {
-    return <WestMidsLCD {...attrs} />;
-  } else if (boardName === 'class-700') {
-    // Hidden option
-    return <Class700PIS />;
-  } else {
-    return null;
-  }
 }
-
-export default IndexPage;
