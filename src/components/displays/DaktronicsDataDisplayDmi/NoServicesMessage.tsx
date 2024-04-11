c515fdddb9e51c7fa23fdac625685788f98cf657
David Wheatley <david@davwheat.dev>
2024-04-11 16:09:31 +0200

    fix: wrong layout on no services message

--- src/components/displays/DaktronicsDataDisplayDmi/NoServicesMessage.tsx
+++ src/components/displays/DaktronicsDataDisplayDmi/NoServicesMessage.tsx
@@ -2,18 +2,10 @@ import React from 'react';
 
 export default function NoServicesMessage() {
   return (
-    <div
-      css={{
-        textAlign: 'center',
-        display: 'flex',
-        flexGrow: '1',
-        flexDirection: 'column',
-        gap: 'var(--gap)',
-      }}
-    >
+    <>
       <div css={{ height: 'var(--row-height)' }} />
-      <div css={{ justifyContent: 'center', height: 'var(--row-height)' }}>CUSTOMER INFORMATION SYSTEM</div>
+      <div css={{ justifyContent: 'center', height: 'var(--row-height)', textAlign: 'center' }}>CUSTOMER INFORMATION SYSTEM</div>
       <div css={{ height: 'var(--row-height)' }} />
-    </div>
+    </>
   );
 }
